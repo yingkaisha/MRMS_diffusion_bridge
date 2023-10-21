df96a3620d2a4ab113a6712a013ca3b822010cc8
Yingkai Sha <yingkaisha@gmail.com>
2023-10-20 22:03:38 -0600

    update the GFS script

--- scripts/DATA01_GFS_save.py
+++ scripts/DATA01_GFS_save.py
@@ -31,6 +31,16 @@ T800 = np.empty((len(date_list),)+shape_grid)
 U800 = np.empty((len(date_list),)+shape_grid)
 RH800 = np.empty((len(date_list),)+shape_grid)
 
+var_inds1 = [315, 320, 316, 451, 474, 476,]
+var_inds2 = [446, 452, 447, 597, 624, 626,]
+
+var_names = [':Temperature:K (instant):regular_ll:isobaricInhPa:level 80000 Pa:fcst time',
+             ':U component of wind:m s**-1 (instant):regular_ll:isobaricInhPa:level 8000',
+             ':Relative humidity:% (instant):regular_ll:isobaricInhPa:level 80000 Pa:fcs',
+             ':Total Precipitation:kg m**-2 (accum):regular_ll:surface:level 0:fcst time',
+             ':Convective available potential energy:J kg**-1 (instant):regular_ll:surfa',
+             ':Precipitable water:kg m**-2 (instant):regular_ll:atmosphereSingleLayer:le']
+
 LEADs  =[3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
 
 for lead in LEADs:
@@ -38,14 +48,35 @@ for lead in LEADs:
     
         filename_gfs = datetime.strftime(dt, 
             '/glade/campaign/collections/rda/data/ds084.1/%Y/%Y%m%d/gfs.0p25.%Y%m%d%H.f{:03d}.grib2'.format(lead))
-    
+
+        # ========== check grb ids ========== #
+        names1 = []
+        names2 = []
+        
         with pygrib.open(filename_gfs) as grbio:
-            T_ = grbio[314+1].values #
-            U_ = grbio[319+1].values
-            RH_ = grbio[315+1].values
-            APCP_ = grbio[450+1].values
-            CAPE_ = grbio[473+1].values
-            PWAT_ = grbio[475+1].values
+            for i in var_inds1:
+                names1.append(str(grbio[i])[3:77])
+            for i in var_inds2:
+                try:
+                    names2.append(str(grbio[i])[3:77])
+                except:
+                    print('list too long')
+        
+        if names1 == var_names:
+            var_inds = var_inds1
+        elif names2 == var_names:
+            var_inds = var_inds2
+        else:
+            print(filename_gfs)
+            error
+
+        with pygrib.open(filename_gfs) as grbio:
+            T_ = grbio[var_inds[0]].values #
+            U_ = grbio[var_inds[1]].values
+            RH_ = grbio[var_inds[2]].values
+            APCP_ = grbio[var_inds[3]].values
+            CAPE_ = grbio[var_inds[4]].values
+            PWAT_ = grbio[var_inds[5]].values
         
         T_NA = T_[:-360, 720:]
         T_NA = np.flipud(np.concatenate((T_NA, T_[:-360, 0][:, None]), axis=1))
@@ -74,7 +105,7 @@ for lead in LEADs:
         U800[i_dt, ...] = U_NA
         RH800[i_dt, ...] = RH_NA
     
-        tuple_save = (APCP, CAPE, PWAT, T800, U800, RH800)
-        label_save = ['APCP', 'CAPE', 'PWAT', 'T800', 'U800', 'RH800']
-    
-        du.save_hdf5(tuple_save, label_save, '/glade/campaign/cisl/aiml/ksha/GFS/', 'GFS_{}_f{:02d}.hdf'.format(year, lead))
+    tuple_save = (APCP, CAPE, PWAT, T800, U800, RH800)
+    label_save = ['APCP', 'CAPE', 'PWAT', 'T800', 'U800', 'RH800']
+
+    du.save_hdf5(tuple_save, label_save, '/glade/campaign/cisl/aiml/ksha/GFS/', 'GFS_{}_f{:02d}.hdf'.format(year, lead))
