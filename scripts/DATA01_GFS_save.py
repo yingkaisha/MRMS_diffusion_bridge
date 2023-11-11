6fb5911cb30eca91e13e1b54dd1e1c3ffcbbc1b4
Yingkai Sha <yingkaisha@gmail.com>
2023-11-10 22:25:32 -0700

    update scripts

--- scripts/DATA01_GFS_save.py
+++ scripts/DATA01_GFS_save.py
@@ -24,90 +24,102 @@ date_list = [base + timedelta(days=h) for h in range(365)]
 
 shape_grid = lon_GFS.shape
 
-APCP = np.empty((len(date_list),)+shape_grid); APCP[...] = np.nan
-CAPE = np.empty((len(date_list),)+shape_grid); CAPE[...] = np.nan
-PWAT = np.empty((len(date_list),)+shape_grid); PWAT[...] = np.nan
-T800 = np.empty((len(date_list),)+shape_grid); T800[...] = np.nan
-U800 = np.empty((len(date_list),)+shape_grid); U800[...] = np.nan
-RH800 = np.empty((len(date_list),)+shape_grid); RH800[...] = np.nan
 
-var_inds1 = [315, 320, 316, 451, 474, 476,]
-var_inds2 = [446, 452, 447, 597, 624, 626,]
+# T800 U800 V800, RH800, APCP, CAPE, PWAT
+var_inds1 = [315, 320, 321, 316, 451, 474, 476,]
+var_inds2 = [446, 452, 453, 447, 597, 624, 626,]
 
 var_names = [':Temperature:K (instant):regular_ll:isobaricInhPa:level 80000 Pa:fcst time',
              ':U component of wind:m s**-1 (instant):regular_ll:isobaricInhPa:level 8000',
+             ':V component of wind:m s**-1 (instant):regular_ll:isobaricInhPa:level 8000',
              ':Relative humidity:% (instant):regular_ll:isobaricInhPa:level 80000 Pa:fcs',
              ':Total Precipitation:kg m**-2 (accum):regular_ll:surface:level 0:fcst time',
              ':Convective available potential energy:J kg**-1 (instant):regular_ll:surfa',
              ':Precipitable water:kg m**-2 (instant):regular_ll:atmosphereSingleLayer:le']
 
 LEADs  =[3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+#INIs = ['00', '06', '12', '18']
+INIs = ['18',]
 
-for lead in LEADs:
-    for i_dt, dt in enumerate(date_list):
+for str_ini in INIs:
+    for lead in LEADs:
+
+        APCP_GFS = np.empty((len(date_list),)+shape_grid); APCP_GFS[...] = np.nan
+        CAPE_GFS = np.empty((len(date_list),)+shape_grid); CAPE_GFS[...] = np.nan
+        PWAT_GFS = np.empty((len(date_list),)+shape_grid); PWAT_GFS[...] = np.nan
+        T800_GFS = np.empty((len(date_list),)+shape_grid); T800_GFS[...] = np.nan
+        U800_GFS = np.empty((len(date_list),)+shape_grid); U800_GFS[...] = np.nan
+        V800_GFS = np.empty((len(date_list),)+shape_grid); V800_GFS[...] = np.nan
+        RH800_GFS = np.empty((len(date_list),)+shape_grid); RH800_GFS[...] = np.nan
+
+        for i_dt, dt in enumerate(date_list):
+        
+            filename_gfs = datetime.strftime(dt, 
+                '/glade/campaign/collections/rda/data/ds084.1/%Y/%Y%m%d/gfs.0p25.%Y%m%d{}.f{:03d}.grib2'.format(str_ini, lead))
     
-        filename_gfs = datetime.strftime(dt, 
-            '/glade/campaign/collections/rda/data/ds084.1/%Y/%Y%m%d/gfs.0p25.%Y%m%d%H.f{:03d}.grib2'.format(lead))
-
-        # ========== check grb ids ========== #
-        names1 = []
-        names2 = []
-        
-        with pygrib.open(filename_gfs) as grbio:
-            for i in var_inds1:
-                names1.append(str(grbio[i])[3:77])
-            for i in var_inds2:
-                try:
-                    names2.append(str(grbio[i])[3:77])
-                except:
-                    print('list too long')
-        
-        if names1 == var_names:
-            var_inds = var_inds1
-        elif names2 == var_names:
-            var_inds = var_inds2
-        else:
-            print(filename_gfs)
-            error_error_error
-
-        with pygrib.open(filename_gfs) as grbio:
-            T_ = grbio[var_inds[0]].values #
-            U_ = grbio[var_inds[1]].values
-            RH_ = grbio[var_inds[2]].values
-            APCP_ = grbio[var_inds[3]].values
-            CAPE_ = grbio[var_inds[4]].values
-            PWAT_ = grbio[var_inds[5]].values
-        
-        T_NA = T[:-360, 720:]
-        T_NA = np.flipud(T_NA)[98:203, 218:460]
-        
-        U_NA = U[:-360, 720:]
-        U_NA = np.flipud(U_NA)[98:203, 218:460]
-        
-        V_NA = V[:-360, 720:]
-        V_NA = np.flipud(V_NA)[98:203, 218:460]
-        
-        RH_NA = RH[:-360, 720:]
-        RH_NA = np.flipud(RH_NA)[98:203, 218:460]
-        
-        APCP_NA = APCP[:-360, 720:]
-        APCP_NA = np.flipud(APCP_NA)[98:203, 218:460]
-        
-        CAPE_NA = CAPE[:-360, 720:]
-        CAPE_NA = np.flipud(CAPE_NA)[98:203, 218:460]
-        
-        PWAT_NA = PWAT[:-360, 720:]
-        PWAT_NA = np.flipud(PWAT_NA)[98:203, 218:460]
-        
-        APCP[i_dt, ...] = APCP_NA
-        CAPE[i_dt, ...] = CAPE_NA
-        PWAT[i_dt, ...] = PWAT_NA
+            # ========== check grb ids ========== #
+            names1 = []
+            names2 = []
+            
+            with pygrib.open(filename_gfs) as grbio:
+                for i in var_inds1:
+                    names1.append(str(grbio[i])[3:77])
+                for i in var_inds2:
+                    try:
+                        names2.append(str(grbio[i])[3:77])
+                    except:
+                        print('list too long')
+            
+            if names1 == var_names:
+                var_inds = var_inds1
+            elif names2 == var_names:
+                var_inds = var_inds2
+            else:
+                print(filename_gfs)
+                print(names1)
+                print(names2)
+                print('error')
+                break;
     
-        T800[i_dt, ...] = T_NA
-        U800[i_dt, ...] = U_NA
-        RH800[i_dt, ...] = RH_NA
+            with pygrib.open(filename_gfs) as grbio:
+                T = grbio[var_inds[0]].values #
+                U = grbio[var_inds[1]].values
+                V = grbio[var_inds[2]].values
+                RH = grbio[var_inds[3]].values
+                APCP = grbio[var_inds[4]].values
+                CAPE = grbio[var_inds[5]].values
+                PWAT = grbio[var_inds[6]].values
+            
+            T_NA = T[:-360, 720:]
+            T_NA = np.flipud(T_NA)[98:203, 218:460]
+            
+            U_NA = U[:-360, 720:]
+            U_NA = np.flipud(U_NA)[98:203, 218:460]
+            
+            V_NA = V[:-360, 720:]
+            V_NA = np.flipud(V_NA)[98:203, 218:460]
+            
+            RH_NA = RH[:-360, 720:]
+            RH_NA = np.flipud(RH_NA)[98:203, 218:460]
+            
+            APCP_NA = APCP[:-360, 720:]
+            APCP_NA = np.flipud(APCP_NA)[98:203, 218:460]
+            
+            CAPE_NA = CAPE[:-360, 720:]
+            CAPE_NA = np.flipud(CAPE_NA)[98:203, 218:460]
+            
+            PWAT_NA = PWAT[:-360, 720:]
+            PWAT_NA = np.flipud(PWAT_NA)[98:203, 218:460]
+            
+            APCP_GFS[i_dt, ...] = APCP_NA
+            CAPE_GFS[i_dt, ...] = CAPE_NA
+            PWAT_GFS[i_dt, ...] = PWAT_NA
+            T800_GFS[i_dt, ...] = T_NA
+            U800_GFS[i_dt, ...] = U_NA
+            V800_GFS[i_dt, ...] = V_NA
+            RH800_GFS[i_dt, ...] = RH_NA
+        
+        tuple_save = (APCP_GFS, CAPE_GFS, PWAT_GFS, T800_GFS, U800_GFS, V800_GFS, RH800_GFS)
+        label_save = ['APCP', 'CAPE', 'PWAT', 'T800', 'U800', 'V800', 'RH800']
     
-    tuple_save = (APCP, CAPE, PWAT, T800, U800, RH800)
-    label_save = ['APCP', 'CAPE', 'PWAT', 'T800', 'U800', 'RH800']
-
-    du.save_hdf5(tuple_save, label_save, '/glade/campaign/cisl/aiml/ksha/GFS/', 'GFS_{}_f{:02d}.hdf'.format(year, lead))
+        du.save_hdf5(tuple_save, label_save, '/glade/campaign/cisl/aiml/ksha/GFS/', 'GFS_{}_ini{}_f{:02d}.hdf'.format(year, str_ini, lead))
